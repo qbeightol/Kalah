02b1c64bd02a7f865895bf2318b8c063e4b01ffb
Quinn Beightol <qeb2@cornell.edu>
2014-11-14 17:33:59 -0500

    changed KalahMove from an enum to a class

--- src/kalah/KalahMove.java
+++ src/kalah/KalahMove.java
@@ -1,31 +1,22 @@
 package kalah;
 
-public enum KalahMove {
-  HOUSE_ONE, 
-  HOUSE_TWO, 
-  HOUSE_THREE, 
-  HOUSE_FOUR, 
-  HOUSE_FIVE, 
-  HOUSE_SIX;
+public class KalahMove {
+  /* represents the number of the pit from which the player begins sowing seeds
+   * invariant: must be an int between 1 and 6, inclusive*/
+  private int houseNumber;
   
-  public static KalahMove of(int n){
-    switch (n) {
-    case 1: 
-      return HOUSE_ONE;
-    case 2: 
-      return HOUSE_TWO;
-    case 3:
-      return HOUSE_THREE;
-    case 4:
-      return HOUSE_FOUR;
-    case 5:
-      return HOUSE_FIVE;
-    case 6:
-      return HOUSE_SIX;
-    default:
-      String msg = "KalahMove.of(int n) received " + n 
-                 + " but expected a number between 1 and 6, inclusive";
+  public KalahMove(int n){
+    if (1 <= n || n <= 6) {
+      houseNumber = n;
+    } else {
+      String msg = "KalahMove(int n) received" + n 
+          + "but expected a number between 1 and 6, inclusive";
       throw new IllegalArgumentException(msg);
     }
   }
+
+  public int getHouseNumber() {
+    return houseNumber;
+  }
+
 }
