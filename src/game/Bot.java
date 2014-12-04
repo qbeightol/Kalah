d4ba3facc4ea3bfeac97c813cc9f59e83e61cea0
Quinn Beightol <qeb2@cornell.edu>
2014-12-04 10:13:23 -0500

    added better documentation and nicer behavior to simpleBot

--- src/game/Bot.java
+++ src/game/Bot.java
@@ -2,7 +2,8 @@ package game;
 
 public interface Bot {
   
-  /* Returns a suggested move for the active player in the provided state */
-  public Move requestMove(State s);
+  /* Returns a suggested move for the active player in the provided state;
+   * Throws an IllegalArgumentException if there are no valid moves from [s] */
+  public Move requestMove(State s) throws IllegalArgumentException;
   
 }
--- src/kalah/SimpleBot.java
+++ src/kalah/SimpleBot.java
@@ -8,12 +8,29 @@ import game.State;
 
 public class SimpleBot implements game.Bot {
   
+  /* returns an integer representing the approximate "goodness" of a move*/
   private int simpleHeuristic(KalahMove move, 
                               KalahState nextState, 
                               Player botPlayer){
-    
+    /* weigh moves that increase your score heavily--if there's a way to capture 
+     * an oponnent's stones or to score a point, this bot will seize that 
+     * opportunity*/
     int scoreValue = nextState.kalahCount(botPlayer) * 1000;
+    
+    /* in the event of a tie between moves that could increase a player's score,
+     * choose a move that grant's them a second turn. Notice that whenever  a 
+     * player takes a move that grants a second turn, their score increases
+     * too. Consequently, this rule and the previous one are designed to prefer
+     * captures over moves that grant a second turn, and to prefer moves that
+     * grant a second turn whenever captures aren't present */
     int secondTurnValue = (nextState.getActivePlayer() ==  botPlayer) ? 100 : 0;
+    
+    /* to help break close ties, we'll favor moves that either clear out a
+     * player's right-most home, or maintain a higher level of free pits
+     * (which, in turn makes it easier to get a capture later on the game). 
+     * As a side note, these two heuristics aren't totally orthogonal, so 
+     * we've given them significantly less weight than the first two, and we're
+     * going to consider an alternate set of heuristics for breaking ties*/
     int distanceToRightValue = move.getHouseNumber();
     int emptyPitsValue = 0;
     
@@ -28,11 +45,13 @@ public class SimpleBot implements game.Bot {
   }
 
   @Override
-  public Move requestMove(State inputState) {
+  public Move requestMove(State inputState) throws IllegalArgumentException{
     
     Move currentResultMove = null;
     int currentResultValue = 0;
     Player botPlayer = inputState.getActivePlayer();
+    
+    /* find the best move according to our simple heuristic, and return it */
     
     for (Entry<Move, State> e : inputState.successors().entrySet()) {
       KalahMove move = (KalahMove) e.getKey();
@@ -44,7 +63,14 @@ public class SimpleBot implements game.Bot {
       }
     }
     
-    return currentResultMove;
+    if (currentResultMove != null) {
+      return currentResultMove;
+    } else {
+      String msg = "No legal moves (you may have provided a terminal state to "
+                 + "this bot)";
+      throw new IllegalArgumentException(msg);
+    }
+    
   }
 
 }
