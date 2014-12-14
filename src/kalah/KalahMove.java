3e2af79ce708d2dedb5648ca70fbd45c41e69ef6
Quinn Beightol <qeb2@cornell.edu>
2014-12-14 08:05:31 -0500

    added a comment explaining the change in the KalahMove implementation?

--- src/kalah/KalahMove.java
+++ src/kalah/KalahMove.java
@@ -3,6 +3,25 @@ package kalah;
 import game.Move;
 
 /* represents the number of the pit from which the player begins sowing seeds */
+
+/* note: it may seem silly to have KalahMove be an enum when it could easily be
+ * a class with a single private int field, but I ran into some issues with 
+ * equality checking in KalahGame (specifically, the applyMove(...) method 
+ * generates a set of valid moves for the current state, and checks whether the
+ * supplied move is one of those moves using contains(...) and consequently 
+ * equals(...)). 
+ * 
+ *  I'm not sure exactly what went wrong when I tried to over-write equality 
+ *  for the more natural, single-field class. I'm guessing it had to do with
+ *  the fact that applyMove(...) expects a Move argument--not a kalah move--and
+ *  therefore used the default equality comparison, pointer equality. I 
+ *  then tried switching to an enum class (which is a reasonably painless way 
+ *  to ensure that there is exactly one object for each type of move, and in
+ *  turn means that whenever you see FOUR, its a reference to the same object).
+ *  Things worked a lot more smoothly, though I'm not sure if the problem was
+ *  triggered because the move was cast to Move--in which case, I could have 
+ *  done a downcast to KalahMove and fixed the issue--or if the problem was 
+ *  more nuanced. */
 public enum KalahMove implements Move{
   ONE, TWO, THREE, FOUR, FIVE, SIX;
 
