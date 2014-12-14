826b6f923926037f483cbe24bb8051adf35a9555
Quinn Beightol <qeb2@cornell.edu>
2014-12-14 16:58:50 -0500

    started to work on testing

--- src/kalah/Main.java
+++ src/kalah/Main.java
@@ -1,5 +1,9 @@
 package kalah;
 
+import game.Player;
+import game.RandomBot;
+import game.State;
+
 import java.util.Arrays;
 
 
@@ -8,9 +12,23 @@ public class Main {
   public static void main(String[] args) {
     
     KalahGame kg = new KalahGame();
+    RandomBot p1 = new RandomBot();
+    RandomBot p2 = new RandomBot();
+    
+    while (!kg.currentKalahState().isTerminal()){
+      State currentState = kg.currentState();
+      if (currentState.getActivePlayer() == Player.ONE){
+        kg.applyMove(p1.requestMove(currentState));
+      } else{
+        kg.applyMove(p2.requestMove(currentState));
+      }
+    }
+    
     kg.printKalahGameInfo();
-    kg.applyMove(KalahMove.ofInt(6));
-    kg.printKalahGameInfo();
+    
+    //kg.printKalahGameInfo();
+    //kg.applyMove(KalahMove.ofInt(6));
+    //kg.printKalahGameInfo();
 
 	  KalahGUI mankalah = new KalahGUI();
       mankalah.setVisible(true);
